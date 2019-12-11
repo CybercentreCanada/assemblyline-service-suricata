e7209f7d6a7b9244fe20f4e6c515176bc5f87620
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-12-11 10:45:56 -0500

    Finished signature importer

--- suricata_/suricata_importer.py
+++ suricata_/suricata_importer.py
@@ -11,7 +11,7 @@ UPDATE_CONFIGURATION_PATH = os.environ.get('UPDATE_CONFIGURATION_PATH', None)
 DEFAULT_STATUS = "TESTING"
 
 
-class SuricataImporter(object):
+class SuricataImporter:
     def __init__(self, al_client, logger=None):
         if not logger:
             from assemblyline.common import log as al_log
@@ -24,69 +24,22 @@ class SuricataImporter(object):
         self.classification = forge.get_classification()
         self.log = logger
 
-    # @staticmethod
-    # def get_signature_name(signature):
-    #     name = None
-    #     for line in signature.splitlines():
-    #         line = line.strip()
-    #         if line.startswith("rule ") or line.startswith("private rule ") \
-    #                 or line.startswith("global rule ") or line.startswith("global private rule "):
-    #             name = line.split(":")[0].split("{")[0]
-    #             name = name.replace("global ", "").replace("private ", "").replace("rule ", "")
-    #             break
-    #
-    #     if name is None:
-    #         return name
-    #     return name.strip()
-
-    # @staticmethod
-    # def parse_meta(signature):
-    #     meta = {}
-    #     meta_started = False
-    #     for line in signature.splitlines():
-    #         line = line.strip()
-    #         if not meta_started and line.startswith('meta') and line.endswith(':'):
-    #             meta_started = True
-    #             continue
-    #
-    #         if meta_started:
-    #             if line.startswith("//") or line == "":
-    #                 continue
-    #
-    #             if "=" not in line:
-    #                 break
-    #
-    #             key, val = line.split("=", 1)
-    #             key = key.strip()
-    #             val = val.strip().strip('"')
-    #             meta[key] = safe_str(val)
-    #
-    #     return meta
-
     def _save_signatures(self, signatures: List[Rule], source, default_status=DEFAULT_STATUS):
         saved_sigs = []
         order = 1
         for signature in signatures:
-            # name = self.get_signature_name(signature)
-            # classification = meta.get('classification', self.classification.UNRESTRICTED)
-
-            # status = meta.get('status', meta.get('al_status', default_status))
-
-            # # Convert CCCS YARA status to AL signature status
-            # if status == "RELEASED":
-            #     status = "DEPLOYED"
-            # elif status == "DEPRECATED":
-            #     status = "DISABLED"
+            name = signature.sid
+            status = "DEPLOYED" if signature.enabled else default_status
 
             sig = Signature(dict(
                 classification=self.classification.UNRESTRICTED,
                 data=signature.raw,
-                name=signature.sid,
+                name=name,
                 order=order,
                 revision=int(float(signature.rev)),
-                signature_id=signature.sid,
+                signature_id=name,
                 source=source,
-                status=default_status,
+                status=status,
                 type="suricata",
             ))
             r = self.update_client.signature.add_update(sig.as_primitives())
@@ -102,46 +55,10 @@ class SuricataImporter(object):
 
         return saved_sigs
 
-    # @ staticmethod
-    # def _split_signatures(data):
-    #     from suricata.update.rule import parse_file
-    #     parse_file()
-    #
-    #     current_signature = []
-    #     signatures = []
-    #     in_rule = False
-    #     for line in data.splitlines():
-    #         temp_line = line.strip()
-    #
-    #         if in_rule:
-    #             current_signature.append(line)
-    #
-    #             if temp_line == "}":
-    #                 signatures.append("\n".join(current_signature))
-    #                 current_signature = []
-    #                 in_rule = False
-    #         else:
-    #             if temp_line.startswith("import ") \
-    #                     or temp_line.startswith("rule ") \
-    #                     or temp_line.startswith("private rule ") \
-    #                     or temp_line.startswith("global rule ") \
-    #                     or temp_line.startswith("global private rule "):
-    #                 in_rule = True
-    #                 current_signature.append(line)
-    #
-    #     return signatures
-
-    # def import_data(self, yara_bin, source, default_status=DEFAULT_STATUS):
-    #     return self._save_signatures(self._split_signatures(yara_bin), source, default_status=default_status)
-
     def import_file(self, file_path: str, source: str, default_status=DEFAULT_STATUS):
         cur_file = os.path.expanduser(file_path)
         if os.path.exists(cur_file):
             signatures = parse_file(cur_file)
             return self._save_signatures(signatures, source, default_status=default_status)
-
-            # with open(cur_file, "r") as yara_file:
-            #     yara_bin = yara_file.read()
-            #     return self.import_data(yara_bin, source or os.path.basename(cur_file), default_status=default_status)
         else:
             raise Exception(f"File {cur_file} does not exists.")
