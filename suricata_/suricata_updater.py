ae67427518b482e4d3e7739bb90f5f9d70b4c5f1
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-12-16 07:53:22 -0500

    Working suricata updater

--- suricata_/suricata_updater.py
+++ suricata_/suricata_updater.py
@@ -4,62 +4,29 @@ import logging
 import os
 import re
 import shutil
-import subprocess
 import tempfile
 import time
 from typing import List, Dict, Any, Optional
 from urllib.parse import urlparse
+from zipfile import ZipFile
 
 import requests
 import yaml
-import yara
+from assemblyline_client import Client
 from git import Repo
 
 from assemblyline.common import log as al_log
 from assemblyline.common.digests import get_sha256_for_file
 from assemblyline.common.isotime import now_as_iso
+from suricata_.suricata_importer import SuricataImporter
 
 al_log.init_logging('service_updater')
 
 LOGGER = logging.getLogger('assemblyline.service_updater')
 
-
 UPDATE_CONFIGURATION_PATH = os.environ.get('UPDATE_CONFIGURATION_PATH', None)
 UPDATE_OUTPUT_PATH = os.environ.get('UPDATE_OUTPUT_PATH', None)
-UPDATE_DIR = os.path.join(tempfile.gettempdir(), 'yara_updates')
-
-
-def _compile_rules(rules_file):
-    """
-    Saves Yara rule content to file, validates the content with Yara Validator, and uses Yara python to compile
-    the rule set.
-
-    Args:
-        rules_file: Yara rule file content.
-
-    Returns:
-        Compiled rules, compiled rules md5.
-    """
-    try:
-        validate = YaraValidator(logger=LOGGER)
-        edited = validate.validate_rules(rules_file)
-    except Exception as e:
-        raise e
-    # Grab the final output if Yara Validator found problem rules
-    # if edited:
-        # with open(rules_file, 'r') as f:
-        #     sdata = f.read()
-        # first_line, clean_data = sdata.split('\n', 1)
-        # if first_line.startswith(prefix):
-        #     last_update = first_line.replace(prefix, '')
-        # else:
-        #     last_update = now_as_iso()
-        #     clean_data = sdata
-
-    # Try to compile the final/cleaned yar file
-    rules = yara.compile(rules_file)
-
-    return True
+UPDATE_DIR = os.path.join(tempfile.gettempdir(), 'suricata_updates')
 
 
 def url_download(source: Dict[str, Any], previous_update: Optional[float] = None) -> (List, List):
@@ -107,12 +74,23 @@ def url_download(source: Dict[str, Any], previous_update: Optional[float] = None
             # File has not been modified since last update, do nothing
             return
         elif response.ok:
-            file_name = os.path.basename(urlparse(uri).path) # TODO: make filename as source name with extension .yar
+            file_name = os.path.basename(urlparse(uri).path)
             file_path = os.path.join(UPDATE_DIR, file_name)
             with open(file_path, 'wb') as f:
                 f.write(response.content)
 
-            return [file_path], [get_sha256_for_file(file_path)]
+            rules_files = None
+            if file_name.endswith('tar.gz'):
+                extract_dir = os.path.join(UPDATE_DIR, name)
+                shutil.unpack_archive(file_path, extract_dir=extract_dir)
+
+                rules_files = set()
+                for path_in_dir, _, files in os.walk(extract_dir):
+                    for filename in files:
+                        filepath = os.path.join(extract_dir, path_in_dir, filename)
+                        rules_files.add(filepath)
+
+            return list(rules_files) or [file_path], [get_sha256_for_file(file_path)]
     except requests.Timeout:
         # TODO: should we retry?
         pass
@@ -129,7 +107,7 @@ def git_clone_repo(source: Dict[str, Any]) -> List[str] and List[str]:
     name = source['name']
     url = source['uri']
     pattern = source.get('pattern', None)
-    key = source.get('public_key', None)
+    key = source.get('private_key', None)
 
     clone_dir = os.path.join(UPDATE_DIR, name)
     if os.path.exists(clone_dir):
@@ -180,10 +158,6 @@ def suricata_update() -> None:
     update_start_time = now_as_iso()
     files_sha256 = []
 
-    # al_combined_yara_rules_dir = os.path.join(tempfile.gettempdir(), 'al_combined_yara_rules')
-    # if not os.path.exists(al_combined_yara_rules_dir):
-    #     os.makedirs(al_combined_yara_rules_dir)
-
     suricata_sources = []
     # Go through each source and download file
     for source_name, source in sources.items():
@@ -213,62 +187,54 @@ def suricata_update() -> None:
         shutil.rmtree(UPDATE_OUTPUT_PATH, ignore_errors=True)
         exit()
 
-    # We're using the '--no-test' mode because otherwise a rule failing causes *no* updates to happen
-    # A few rules typically fail out of the box because the default value for HOME_NET is 'any'
-    # and some rules check for !$HOME_NET - which suricata errors on
-    output_path = os.path.join(UPDATE_OUTPUT_PATH, 'suricata.rules')
-    command = ["suricata-update", f"-o {output_path}", "--no-test"]
-    for rules_source in suricata_sources:
-        if rules_source[0] == 'url':
-            command.extend(["--url", rules_source[1]])
-        elif rules_source[0] == 'local':
-            command.extend(["--local", rules_source[1]])
-    subprocess.call(command)
-    # subprocess.call(["touch", self.oinkmaster_update_file])
+    # # We're using the '--no-test' mode because otherwise a rule failing causes *no* updates to happen
+    # # A few rules typically fail out of the box because the default value for HOME_NET is 'any'
+    # # and some rules check for !$HOME_NET - which suricata errors on
+    # command = ["suricata-update", f"-D /tmp/suricata_data_dir", f"-o {os.path.join(UPDATE_OUTPUT_PATH, 'rules')}", "--no-test", "--force", "--verbose"]
+    # for rules_source in suricata_sources:
+    #     if rules_source[0] == 'url':
+    #         command.extend(["--url", rules_source[1]])
+    #     elif rules_source[0] == 'local':
+    #         command.extend(["--local", rules_source[1]])
+    # LOGGER.info(command)
+    # subprocess.call(command)
+    # # subprocess.call(["touch", self.oinkmaster_update_file])
+
+    files = glob.glob(UPDATE_OUTPUT_PATH+'/*')
+    LOGGER.info(files)
 
     LOGGER.info("Suricata rule(s) file(s) successfully downloaded")
 
-    # server = update_config['ui_server']
-    # user = update_config['api_user']
-    # api_key = update_config['api_key']
-    # al_client = Client(server, apikey=(user, api_key), verify=False)
+    server = update_config['ui_server']
+    user = update_config['api_user']
+    api_key = update_config['api_key']
+    al_client = Client(server, apikey=(user, api_key), verify=False)
 
-    # yara_importer = YaraImporter(al_client)
+    suricata_importer = SuricataImporter(al_client)
 
-    # for x in os.listdir(al_combined_yara_rules_dir):
-    #     source_name = os.path.splitext(os.path.basename(x))[0]
-    #
-    #     tmp_dir = tempfile.mkdtemp(dir='/tmp')
-    #     try:
-    #         rules_file = os.path.join(tmp_dir, 'rules.yar')
-    #         with open(rules_file, 'w') as f:
-    #             f.write(open(os.path.join(al_combined_yara_rules_dir, x), 'r').read())
-    #
-    #         _compile_rules(rules_file)
-    #         yara_importer.import_file(rules_file, source_name)
-    #     except Exception as e:
-    #         raise e
-    #     finally:
-    #         shutil.rmtree(tmp_dir)
+    for path_in_dir, _, files in os.walk(UPDATE_DIR):
+        for filename in files:
+            source_name = os.path.splitext(os.path.basename(filename))[0]
+            filepath = os.path.join(UPDATE_DIR, path_in_dir, filename)
+            suricata_importer.import_file(filepath, source_name)
 
-    # TODO: Download all signatures matching query and unzip received file to UPDATE_OUTPUT_PATH
-    # previous_update = update_config.get('previous_update', '')
-    # if al_client.signature.update_available(since=previous_update, sig_type='yara')['update_available']:
-    #     LOGGER.info("AN UPDATE IS AVAILABLE TO DOWNLOAD")
+    previous_update = update_config.get('previous_update', '')
+    if al_client.signature.update_available(since=previous_update, sig_type='suricata')['update_available']:
+        LOGGER.info("AN UPDATE IS AVAILABLE TO DOWNLOAD")
 
-        # temp_zip_file = os.path.join(UPDATE_OUTPUT_PATH, 'temp.zip')
-        # al_client.signature.download(output=temp_zip_file, query="type:yara AND (status:TESTING OR status:DEPLOYED)")
+        temp_zip_file = os.path.join(UPDATE_OUTPUT_PATH, 'temp.zip')
+        al_client.signature.download(output=temp_zip_file, query="type:suricata AND (status:TESTING OR status:DEPLOYED)")
 
-        # if os.path.exists(temp_zip_file):
-        #     with ZipFile(temp_zip_file, 'r') as zip_f:
-        #         zip_f.extractall(UPDATE_OUTPUT_PATH)
-        #
-        #     os.remove(temp_zip_file)
+        if os.path.exists(temp_zip_file):
+            with ZipFile(temp_zip_file, 'r') as zip_f:
+                zip_f.extractall(UPDATE_OUTPUT_PATH)
 
-    # combined_suricata_rules_path = '/var/lib/suricata/rules/suricata.rules'
-    # output_path = os.path.join(UPDATE_OUTPUT_PATH, 'suricata.rules')
-    # if os.path.exists(combined_suricata_rules_path):
-    #     shutil.copyfile(combined_suricata_rules_path, output_path)
+            os.remove(temp_zip_file)
+
+    combined_suricata_rules_path = '/var/lib/suricata/rules/suricata.rules'
+    output_path = os.path.join(UPDATE_OUTPUT_PATH, 'suricata.rules')
+    if os.path.exists(combined_suricata_rules_path):
+        shutil.copyfile(combined_suricata_rules_path, output_path)
 
     # Create the response yaml
     with open(os.path.join(UPDATE_OUTPUT_PATH, 'response.yaml'), 'w') as yml_fh:
