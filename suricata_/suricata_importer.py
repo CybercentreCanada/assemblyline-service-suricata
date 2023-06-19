62db415f12c471750f10f900f1524c0d341326e6
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-06-19 18:48:58 +0000

    Handle cases where there was no metadata originally in rule + black formatting

--- suricata_/suricata_importer.py
+++ suricata_/suricata_importer.py
@@ -7,17 +7,18 @@ from assemblyline.common import forge
 from assemblyline.odm.models.signature import Signature
 from suricata.update.rule import Rule, parse_file
 
-UPDATE_CONFIGURATION_PATH = os.environ.get('UPDATE_CONFIGURATION_PATH', None)
+UPDATE_CONFIGURATION_PATH = os.environ.get("UPDATE_CONFIGURATION_PATH", None)
 
-BATCH_SIZE_LIMIT = int(os.environ.get('SIG_BATCH_SIZE', 1000))
+BATCH_SIZE_LIMIT = int(os.environ.get("SIG_BATCH_SIZE", 1000))
 
 
 class SuricataImporter:
     def __init__(self, al_client, logger=None):
         if not logger:
             from assemblyline.common import log as al_log
-            al_log.init_logging('suricata_importer')
-            logger = logging.getLogger('assemblyline.suricata_importer')
+
+            al_log.init_logging("suricata_importer")
+            logger = logging.getLogger("assemblyline.suricata_importer")
             logger.setLevel(logging.INFO)
 
         self.update_client = al_client
@@ -38,32 +39,43 @@ class SuricataImporter:
             # Update metadata to include reference to signature in Assemblyline
             orig_meta, new_meta = signature.metadata, deepcopy(signature.metadata)
             new_meta.append(f"al_signature {source}.{name}")
-            new_meta.append(f"classification {classification}")
-            signature.raw = signature.raw.replace(", ".join(orig_meta), ", ".join(new_meta))
+            if not any(m.startswith("classification ") for m in signature.metadata):
+                # If there's no classification metadata, then set it based on the source
+                new_meta.append(f"classification {classification}")
 
-            sig = Signature(dict(
-                classification=classification,
-                data=signature.raw,
-                name=name,
-                order=order,
-                revision=int(float(signature.rev)),
-                signature_id=signature.sid,
-                source=source,
-                status=status,
-                type="suricata",
-            ))
+            if orig_meta:
+                # Replace metadata if there was any metadata originally
+                signature.raw = signature.raw.replace(", ".join(orig_meta), ", ".join(new_meta))
+            else:
+                # Otherwise append metadata to rule
+                signature.raw = signature.raw[:-1] + f" metadata:{', '.join(new_meta)};)"
+
+            sig = Signature(
+                dict(
+                    classification=classification,
+                    data=signature.raw,
+                    name=name,
+                    order=order,
+                    revision=int(float(signature.rev)),
+                    signature_id=signature.sid,
+                    source=source,
+                    status=status,
+                    type="suricata",
+                )
+            )
 
             upload_list.append(sig.as_primitives())
             order += 1
             # If we hit the batch size limit, send to API
             if order % BATCH_SIZE_LIMIT == 0:
-                self.log.info(f'Batch limit reached: {BATCH_SIZE_LIMIT}. Sending batch to Signature API..')
-                order_completed += add_update_many(source, 'suricata', upload_list, dedup_name=False)['success']
+                self.log.info(f"Batch limit reached: {BATCH_SIZE_LIMIT}. Sending batch to Signature API..")
+                order_completed += add_update_many(source, "suricata", upload_list, dedup_name=False)["success"]
                 upload_list = []
 
-        order_completed += add_update_many(source, 'suricata', upload_list, dedup_name=False)['success']
-        self.log.info(f"Imported {order_completed}/{order - 1} signatures"
-                      f" from {os.path.basename(cur_file)} into Assemblyline")
+        order_completed += add_update_many(source, "suricata", upload_list, dedup_name=False)["success"]
+        self.log.info(
+            f"Imported {order_completed}/{order - 1} signatures" f" from {os.path.basename(cur_file)} into Assemblyline"
+        )
 
         return order_completed
 
