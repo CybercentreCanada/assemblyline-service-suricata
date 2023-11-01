40576526ea5b137acf3d54fc7e2e39ed3a34eca8
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-11-01 17:13:19 +0000

    Re-ordering imports

--- suricata_/suricata_.py
+++ suricata_/suricata_.py
@@ -11,18 +11,17 @@ import dateutil.parser as dateparser
 import regex
 import suricatasc
 import yaml
-from assemblyline_service_utilities.common.network_helper import convert_url_to_https
-from assemblyline_v4_service.common.base import ServiceBase
-from assemblyline_v4_service.common.request import MaxExtractedExceeded
-from assemblyline_v4_service.common.result import BODY_FORMAT, Result, ResultSection
-from retrying import RetryError, retry
-
 from assemblyline.common.exceptions import RecoverableError
 from assemblyline.common.forge import get_classification
 from assemblyline.common.str_utils import safe_str
 from assemblyline.odm.base import DOMAIN_ONLY_REGEX, IP_ONLY_REGEX
 from assemblyline.odm.models.ontology.results import NetworkConnection, Signature
-from assemblyline.odm.models.result import PARENT_RELATION
+from assemblyline_v4_service.common.task import PARENT_RELATION
+from assemblyline_service_utilities.common.network_helper import convert_url_to_https
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.request import MaxExtractedExceeded
+from assemblyline_v4_service.common.result import BODY_FORMAT, Result, ResultSection
+from retrying import RetryError, retry
 
 SURICATA_BIN = "/usr/local/bin/suricata"
 Classification = get_classification()
